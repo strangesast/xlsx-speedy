cdaaca6dc0d1e18e5bb755d2c81115444146136c
Sam Zagrobelny <strangesast@gmail.com>
2021-04-28 13:30:17 -0700

    out-of-scope

--- go/go.mod
+++ go/go.mod
@@ -4,5 +4,5 @@ go 1.16
 
 require (
 	github.com/360EntSecGroup-Skylar/excelize v1.4.1 // indirect
-	github.com/360EntSecGroup-Skylar/excelize/v2 v2.4.0 // indirect
+	github.com/360EntSecGroup-Skylar/excelize/v2 v2.4.0
 )
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -4,19 +4,75 @@ use std::time::Instant;
 
 use calamine::{open_workbook, Reader, Xlsx};
 
+const HEADER: [(&str, &str); 18] = [
+    ("CUSTOMER", "customer"),
+    ("TOP ASSY\r\nDESCRIPTION", "top_assembly_description"),
+    ("TOP ASSY \r\nP/N", "top_assembly_part_number"),
+    ("SALES \r\nORDER #", "order_number"),
+    ("QTY", "qty"),
+    ("DUE \r\nDATE", "due_date"),
+    (".", ""),
+    ("PART\r\nDESCRIPTION", "part_description"),
+    ("PART NUMBER", "part_number"),
+    ("MATERIALS", "materials"),
+    ("SAW", "status_saw"),
+    ("LATHE", "status_lathe"),
+    ("MILL", "status_mill"),
+    ("WELD", "status_weld"),
+    ("Outside\r\nService", "status_outside_service"),
+    ("PACK / ASSY", "status_assembly"),
+    ("LOCATION", "location"),
+    ("NOTES", "notes"),
+];
+
+const EMPTY_THRESHOLD: usize = 4;
+
 fn func(filepath: &Path, sheet_name: &str) {
     let mut excel: Xlsx<_> = open_workbook(filepath).unwrap();
     // let mut lastcell = String::new();
 
     // let mut count = 0;
+    // let mut s: String = String::from("");
+    let mut values: Vec<Vec<(&str, String, (usize, usize))>> = vec![];
     if let Some(Ok(r)) = excel.worksheet_range(&sheet_name) {
-        for row in r.rows() {
-            for _ in row.iter() {
-                // let ptr = &cell.to_string();
-                // lastcell.clone_from(ptr);
-                // count += 1;
+        let mut rows = r.rows().enumerate().skip(2);
+
+        // check for a valid header
+        assert!(rows
+            .next()
+            .unwrap()
+            .1
+            .iter()
+            .map(|c| c.to_string())
+            .zip(HEADER.iter().map(|p| p.0))
+            .all(|(a, b)| a == b));
+
+        let header_keys = HEADER.iter().map(|p| p.1);
+
+        let mut empty_cnt = 0;
+        'outer: for (j, row) in rows {
+            let first_cell = row[0].to_string();
+            if first_cell == "" {
+                empty_cnt += 1;
+                if empty_cnt >= EMPTY_THRESHOLD {
+                    break 'outer;
+                }
+                continue;
+            } else {
+                empty_cnt = 0;
             }
+
+            values.push(
+                row.iter()
+                    .enumerate()
+                    .zip(header_keys.clone())
+                    .filter(|(_, h)| *h != "")
+                    .map(|((i, cell), h)| (h, cell.to_string(), (i, j)))
+                    .collect(),
+            )
         }
+
+        println!("length: {}", values.len());
     }
 }
 
@@ -37,5 +93,10 @@ fn timeit(f: &dyn Fn()) -> f64 {
 }
 
 fn main() {
+    // let s = env::var("FILEPATH").unwrap_or("../schedule.xlsm".to_string());
+    // let filepath = Path::new(&s);
+    // // let filepath = Path::new("../schedule.xlsm");
+    // let sheet_name = "PRODUCTION SCHEDULE";
+
     println!("{}", timeit(&test));
 }
